1f874397c827b49a3268fdd2d096646baa00f0e5
lxalxy <lxaljx@163.com>
2015-07-22 16:46:10 +0800

    测试

--- BarcodeApp/Form2.cs
+++ BarcodeApp/Form2.cs
@@ -32,20 +32,6 @@ namespace BarcodeApp
             {
                 sqlcon.Open();
                 MessageBox.Show("Open Database Connect Success");
-                //写入一条数据
-                string strUserName = "作业本";
-                string strPWD = "Ab123456";
-                using (SqlCommand sqlCmd = sqlcon.CreateCommand())
-                {
-                    sqlCmd.CommandText = "insert into ACCOUNT(Username,Password) values (@UserName,@PWD) ";//连接字符串进行参数化
-                    SqlParameter[] sqlPara = new SqlParameter[] { 
-                    new SqlParameter("UserName",strUserName),
-                    new SqlParameter("PWD",strPWD)
-                    };
-                    sqlCmd.Parameters.AddRange(sqlPara); //把Paramerter 数组参数添加到sqlCmd中
-                    sqlCmd.ExecuteNonQuery();
-                    MessageBox.Show("Insert OK");
-                }
             }
         }
     }

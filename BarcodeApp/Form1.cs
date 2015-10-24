e3afabd6d2b6c2da4d05ba9a68ddcdc3e17ad77b
lxalxy <lxaljx@163.com>
2015-10-24 22:58:13 +0800

    修改了无法从数据库中获取ITEM的bug，原因是连接字符串错误（由于开发环境变为VS2015，目录错误）。此外解决了数据库中float数据无法读取的问题。

--- BarcodeApp/Form1.cs
+++ BarcodeApp/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
+using System.Data.SqlClient;
 
 namespace BarcodeApp
 {
@@ -85,7 +86,10 @@ namespace BarcodeApp
         private void comm_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             int n = mycomm.BytesToRead;
-            byte[] buf = new byte[n];//声明一个临时数组存储当前来的串口数据      
+            byte[] buf = new byte[n];//声明一个临时数组存储当前来的串口数据    
+
+            string name = null;
+            string price = null;
 
             mycomm.Read(buf, 0, n);//读取缓冲数据
             builder.Clear();//清除字符串构造器的内容
@@ -107,6 +111,59 @@ namespace BarcodeApp
                 {
                     builder.Append(Encoding.ASCII.GetString(buf));
                 }
+
+                string connStr = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=" + @"C:\Users\lxalxy\Documents\visual studio 2015\Projects\BarcodeApp\BarcodeApp\BarcodeDatabase.mdf" + @";Integrated Security=True;Connect Timeout=30";
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    conn.Open();
+                    MessageBox.Show("数据库打开成功！");
+                    //创建sql 查询语句  
+                    string sql_name = "select NAME from Item where ISBN ='" + builder.ToString() + "'";
+                    string sql_price = "select PRICE from Item where ISBN ='" + builder.ToString() + "'";
+                    //创建 SqlCommand 执行指令
+                    using (SqlCommand cmd = new SqlCommand(sql_name, conn))
+                    {
+                        //使用 SqlDataReader 来 读取数据库  
+                        using (SqlDataReader sdr = cmd.ExecuteReader())
+                        {
+                            //SqlDataReader 在数据库中为 从第1条数据开始 一条一条往下读 
+                            if (sdr.Read())  //如果读取账户成功(文本框中的用户名在数据库中存在)
+                            {
+                                //则将第1条 密码 赋给 字符串pwd  ,并且依次往后读取 所有的密码
+                                //Trim()方法为移除字符串前后的空白
+                                name = sdr.GetString(0).Trim();
+                            }
+                            else
+                            {
+                                //如果读取账户数据失败, 则用户名不存在
+                                MessageBox.Show("ISBN不存在,请重新输入!");
+                            }
+                        }
+                    }
+                    using (SqlCommand cmd = new SqlCommand(sql_price, conn))
+                    {
+                        //使用 SqlDataReader 来 读取数据库  
+                        using (SqlDataReader sdr = cmd.ExecuteReader())
+                        {
+                            //SqlDataReader 在数据库中为 从第1条数据开始 一条一条往下读 
+                            if (sdr.Read())  //如果读取账户成功(文本框中的用户名在数据库中存在)
+                            {
+                                //此处需要注意：SQL中的float对应C#里的double，所以需要用GetDouble方法
+                                price = sdr.GetDouble(0).ToString();
+                            }
+                            else
+                            {
+                                //如果读取账户数据失败, 则用户名不存在
+                                MessageBox.Show("ISBN不存在,请重新输入!");
+                            }
+                        }
+                    }
+                }
+
+                //将上面三个变量合成一个数组
+                string[] row = { name, builder.ToString(), price};
+                //给dataGridView1控件添加数据
+                dataGridView1.Rows.Add(row);
 
                 int lines = textBoxRx.GetLineFromCharIndex(textBoxRx.Text.Length) + 1;
                 if (lines > 30)
